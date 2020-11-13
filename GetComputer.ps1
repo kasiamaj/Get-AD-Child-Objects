75a2dc949d5157b804bc91f967551aceb4b14998
kasiamaj <47868481+kasiamaj@users.noreply.github.com>
2020-11-13 09:53:00 -0600

    Add files via upload

---
+++ GetComputer.ps1
@@ -0,0 +1,4 @@
+﻿ForEach ($computer in (Get-Content "C:\Burwood Powershell Outputs\comps.txt"))
+{
+(Get-ADComputer -Identity $computer).distinguishedname | Out-File -Filepath "C:\Burwood Powershell Outputs\results.txt" -append
+}
---
+++ GetObject.ps1
@@ -0,0 +1,4 @@
+﻿ForEach ($computer in (Get-Content "C:\Burwood Powershell Outputs\results.txt"))
+{
+Get-ADObject -filter * -Searchbase $computer | format-list | Out-File -Filepath "C:\Burwood Powershell Outputs\childobjectresults.txt" -append
+}
